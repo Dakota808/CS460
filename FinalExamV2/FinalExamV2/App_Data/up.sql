5f9617db8e56012ea9582570925a19c59e36eef0
Dakota Koki <Dkoki14@wou.edu>
2017-12-06 21:55:00 -0800

    These are Revisions of some of the labs

--- FinalExamV2/FinalExamV2/App_Data/up.sql
+++ FinalExamV2/FinalExamV2/App_Data/up.sql
@@ -1,65 +1,39 @@
-CREATE TABLE Actors(
-ActorName varchar(50) not null,
-ActorID int IDENTITY(1,1)not null PRIMARY KEY,
+CREATE TABLE (
+
   
 );
 
-CREATE TABLE Directors(
-DirecName varchar(50),
-DirecID int IDENTITY(1,1) not null PRIMARY KEY, 
-);
-
-CREATE TABLE Movie (
-MovieName varchar(50) not null,
-MovieYear varchar(50) not null,
-MovieID int IDENTITY (1,1) not null PRIMARY KEY,
-Director int FOREIGN KEY REFERENCES Directors(DirecID),
+CREATE TABLE (
 
 );
 
-CREATE TABLE CastMem (
-CastID int IDENTITY (1,1) not null PRIMARY KEY,
-castCrew int FOREIGN KEY REFERENCES Actors(ActorID),
-MovieType int FOREIGN KEY REFERENCES Movie(MovieID)
+CREATE TABLE (
+
 );
 
-INSERT INTO Actors(ActorName)
+CREATE TABLE (
+
+);
+
+INSERT INTO Buyers(BuyersName)
     VALUES
-	('Daisy Ridley'),
-	('Andy Serkis'),
-	('Benicio Del Toro'),
-	('Penelope Cruz');
 
 
-INSERT INTO Directors(DirecName)
+INSERT INTO Sellers(SellersName)
     VALUES
-	('Rian Johnson'),
-	('Kenneth Branagh'),
-	('Rob Marshall'),
-	('James Marsh');
 
 
-INSERT INTO Movie(MovieName, MovieYear, Director)
+INSERT INTO Items(itemName, itemDescription, seller)
     VALUES
-	('Star Wars: The Last Jedi', '2017', '1'),
-	('Murder on the Orient Express', '2017','2'),
-	('Priates of the Caribbean: On Stranger Tides', '2011', '3'),
-	('The Theory of Everything', '2014', '4');
 
-INSERT INTO CastMem(castCrew, MovieType)
+INSERT INTO Bids(itemID, bider, prices, timeStamps)
     VALUES
-	('4','3'),
-	('4', '2'),
-	('1', '2'),
-	('3', '1'),
-	('2', '1'),
-	('1','1');
 
 
 GO
 
 -- testing
-select * from Actors;
-select * from Directors;
-select * from Movie JOIN Directors on Movie.Director=Directors.DirecID;
-select castCrew, MovieType from CastMem JOIN Actors on CastMem.castCrew=Actors.ActorID;
+--select * from ;
+--select * from ;
+--select *  from  JOIN Items on =;
+--select  *  from  JOIN Bids on ;
